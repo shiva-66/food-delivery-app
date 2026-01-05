07609410ef6c73501bf606ab7254f4c68c49c8f7
shiva <shivakakarakayala@gmail.com>
2026-01-05 12:17:12 +0530

    update list.jsx
    Removed /api duplication

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -8,7 +8,7 @@ const List = ({url}) => {
   const [list,setList] = useState([]);
 
   const fetchList = async () =>{
-    const response = await axios.get(`${url}/api/food/list`); 
+    const response = await axios.get(`${url}/food/list`); 
       if (response.data.success) {
         setList(response.data.data);
       }
